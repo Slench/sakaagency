15e5dece704269c900dc0f5abc5932656c3d704d
Slench <96989611+Slench@users.noreply.github.com>
2022-04-15 19:13:39 +0300

    Saka

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.testimonials__slider--inner').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:false,
+		speed:1000,
+		autoplaySpeed:800,
+		touchTreshhold:30,
+		responsive:[
+						
+		]
+	});
+});
+
